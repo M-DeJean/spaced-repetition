0a59fc015cd06acfeb253571d58f3ad2d82fee9b
M-DeJean <dejeantech@yahoo.com>
2020-11-09 22:47:20 -0800

    styled both login and registration pages...day 1 complete

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import './DashboardRoute.css'
 
 class DashboardRoute extends Component {
   render() {
     return (
-      <section>
+      <section className="dashboard">
         implement and style me
       </section>
     );
