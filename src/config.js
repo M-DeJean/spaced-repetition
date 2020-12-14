df8224fd996e83fd953a275196887555b0425d3f
M-DeJean <dejeantech@yahoo.com>
2020-12-14 14:37:58 -0800

    changed api endpoint to heroku server

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:9000/api',
-  // API_ENDPOINT: 'https://quiet-woodland-45555.herokuapp.com/api',
+  // API_ENDPOINT: 'http://localhost:9000/api',
+  API_ENDPOINT: 'https://quiet-woodland-45555.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
